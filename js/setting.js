2bff88b8343a0acc4b4e1c19fa9dc36e424f64ca
Junyoung Hong <hjunyoung10@gmail.com>
2021-07-28 11:45:50 +0900

    Add font color setting

--- js/setting.js
+++ js/setting.js
@@ -1,6 +1,29 @@
+const { body } = document;
 const setting = document.querySelector('.setting');
 const settingIcon = document.querySelector('.setting__icon');
 const settingMenu = setting.querySelector('.setting__menu');
+
+const fontSetting = settingMenu.querySelector('.setting--font');
+const settingSaveButton = settingMenu.querySelector('[type=submit]');
+
+const FONT_COLOR_KEY = 'font-color';
+let fontColor;
+
+const storeFontColor = (e) => {
+  fontColor = e.target.value;
+  body.style.color = fontColor;
+};
+
+const saveSetting = (e) => {
+  e.preventDefault();
+  const newFontColor = fontColor;
+  localStorage.setItem(FONT_COLOR_KEY, newFontColor);
+
+  settingMenu.classList.add('deleted');
+};
+
+fontSetting.addEventListener('input', storeFontColor);
+settingSaveButton.addEventListener('click', saveSetting);
 
 const retainSettingMenu = () => {
   if (settingMenu.classList.contains('deleted')) {
@@ -24,3 +47,8 @@ const handleSettingClick = () => {
 setting.addEventListener('mouseenter', handleSettingMouseEnter);
 setting.addEventListener('mouseleave', handleSettingMouseLeave);
 settingIcon.addEventListener('click', handleSettingClick);
+
+window.addEventListener('load', () => {
+  const fontColor = localStorage.getItem(FONT_COLOR_KEY);
+  body.style.color = fontColor;
+});
