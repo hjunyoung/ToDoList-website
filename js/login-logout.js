a7f5f341cef5d0f56c57014a3faccf5ad176f595
Junyoung Hong <hjunyoung10@gmail.com>
2021-07-29 18:27:12 +0900

    Add logout modal

--- js/login-logout.js
+++ js/login-logout.js
@@ -1,25 +1,26 @@
-const loginContainer = document.querySelector(".login");
-const loginForm = loginContainer.querySelector(".login__form");
-const loginInput = loginForm.querySelector("#login__form__input");
-const greeting = document.querySelector(".greeting");
-const userGreeting = greeting.querySelector(".greeting__user");
-const logoutButton = greeting.querySelector(".logout");
+/* eslint-disable no-undef */
+const loginContainer = document.querySelector('.login');
+const loginForm = loginContainer.querySelector('.login__form');
+const loginInput = loginForm.querySelector('#login__form__input');
+const greeting = document.querySelector('.greeting');
+const userGreeting = greeting.querySelector('.greeting__user');
+const logoutButton = greeting.querySelector('.logout');
 
-const DELETED_CLASS = "deleted";
-const USERNAME_KEY = "username";
+const DELETED_CLASS = 'deleted';
+const USERNAME_KEY = 'username';
 
 const determineGreeting = () => {
   let greeting;
   const hour = new Date().getHours();
 
   if (hour >= 5 && hour < 12) {
-    greeting = "Good morning";
+    greeting = 'Good morning';
   } else if (hour >= 12 && hour < 18) {
-    greeting = "Good afternoon";
+    greeting = 'Good afternoon';
   } else if (hour >= 18 && hour < 24) {
-    greeting = "Good evening";
+    greeting = 'Good evening';
   } else {
-    greeting = "Sleep tight";
+    greeting = 'Sleep tight';
   }
   return greeting;
 };
@@ -34,35 +35,50 @@ const paintUserGreeting = (username) => {
 const handleLoginSubmit = (e) => {
   e.preventDefault();
   const username = loginInput.value.trim();
-  loginInput.setCustomValidity("");
+  loginInput.setCustomValidity('');
 
   if (username) {
     localStorage.setItem(USERNAME_KEY, username);
     paintUserGreeting(username);
     window.location.reload();
   } else {
-    loginInput.setCustomValidity("Only spaces are not allowed");
+    loginInput.setCustomValidity('Only spaces are not allowed');
   }
   loginInput.reportValidity();
-  loginInput.value = "";
+  loginInput.value = '';
   loginInput.focus();
 };
 
 const handleKeyDownSubmit = () => {
-  loginInput.setCustomValidity("");
+  loginInput.setCustomValidity('');
 };
 
 const handleLogout = (e) => {
   e.preventDefault();
-  const logout = window.confirm(
-    "Your todolist will be deleted. Are you sure you want to logout?"
-  );
-  if (logout) {
-    window.location.reload();
-    localStorage.clear();
-    loginContainer.classList.remove(DELETED_CLASS);
-    greeting.classList.add(DELETED_CLASS);
-  }
+  const logoutModal = document.querySelector('.modal');
+  logoutModal.classList.remove('deleted');
+
+  const modalContent = document.querySelector('.modal__content');
+  const buttonArea = document.querySelector('.modal__button');
+  const confirmMessage = document.createElement('p');
+  const yesButton = document.createElement('input');
+  const noButton = document.createElement('input');
+
+  yesButton.type = 'submit';
+  yesButton.value = 'Yes';
+  noButton.type = 'submit';
+  noButton.value = 'No';
+
+  confirmMessage.innerText =
+    'Your todolist will be deleted. \n Are you sure you want to logout?';
+
+  modalContent.appendChild(confirmMessage);
+  buttonArea.appendChild(yesButton);
+  buttonArea.appendChild(noButton);
+
+  yesButton.addEventListener('click', handleLogoutModal);
+  noButton.addEventListener('click', closeModal);
+  window.addEventListener('keydown', ecsModalClose);
 };
 
 const handleLoad = (e) => {
@@ -70,12 +86,12 @@ const handleLoad = (e) => {
   if (localUsername) {
     paintUserGreeting(localUsername);
     setInterval(paintUserGreeting, 1000, localUsername);
-    logoutButton.addEventListener("click", handleLogout);
+    logoutButton.addEventListener('click', handleLogout);
   } else {
     loginContainer.classList.remove(DELETED_CLASS);
-    loginForm.addEventListener("submit", handleLoginSubmit);
-    loginForm.addEventListener("keydown", handleKeyDownSubmit);
+    loginForm.addEventListener('submit', handleLoginSubmit);
+    loginForm.addEventListener('keydown', handleKeyDownSubmit);
   }
 };
 
-window.addEventListener("load", handleLoad);
+window.addEventListener('load', handleLoad);
--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,39 @@
 /* eslint-disable no-unused-vars */
+const clearModal = () => {
+  const modalMessage = document.querySelector('.modal p');
+  modalMessage.remove();
+
+  const modalButtons = document.querySelectorAll('.modal input');
+  for (let i = 0; i < modalButtons.length; i++) {
+    modalButtons[i].remove();
+  }
+};
+
 const closeModal = (e) => {
   e.preventDefault();
   const targetModal = e.target.parentNode.parentNode.parentNode;
   targetModal.classList.add('deleted');
+
+  clearModal();
 };
 
 const ecsModalClose = (e) => {
   if (e.key === 'Escape') {
     const targetModal = document.querySelector('.modal');
     targetModal.classList.add('deleted');
+    clearModal();
   }
+};
+
+const handleLogoutModal = (e) => {
+  e.preventDefault();
+  const loginContainer = document.querySelector('.login');
+  const greeting = document.querySelector('.greeting');
+  const todo = document.querySelector('.todo');
+
+  window.location.reload();
+  loginContainer.classList.remove('deleted');
+  todo.classList.add('deleted');
+  greeting.classList.add('deleted');
+  localStorage.clear();
 };
