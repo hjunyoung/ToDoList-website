999022c91f012013059dfbce5374af281752c0f5
Junyoung Hong <hjunyoung10@gmail.com>
2021-08-04 12:01:49 +0900

    Add style to calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,14 @@
 const calendar = document.querySelector('.calendar');
+const calendarDay = calendar.querySelector('.calendar--day .calendar__content');
+const calendarMonth = calendar.querySelector(
+  '.calendar--month .calendar__content'
+);
+const calendarDate = calendar.querySelector(
+  '.calendar--date .calendar__content'
+);
+const calendarYear = calendar.querySelector(
+  '.calendar--year .calendar__content'
+);
 
 const getDate = () => {
   const options = {
@@ -7,9 +17,16 @@ const getDate = () => {
     day: 'numeric',
     year: 'numeric',
   };
-  const now = new Date();
-  const date = now.toLocaleString('en-us', options);
-  calendar.innerText = date;
+  const now = new Date().toLocaleString('en-us', options);
+  const day = now.slice(0, 3);
+  const month = now.slice(5, 8);
+  const date = now.slice(-8, -6).trim();
+  const year = now.slice(-4);
+
+  calendarDay.innerHTML = `<p>${day}</p>`;
+  calendarMonth.innerHTML = `<p>${month}</p>`;
+  calendarDate.innerHTML = `<p>${date}</p>`;
+  calendarYear.innerHTML = `<p>${year}</p>`;
 };
 
 getDate();
